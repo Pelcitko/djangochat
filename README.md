2695aa41002a03f09886720eedaa9e6b22a222a7
Áš <pelc.it@gmail.com>
2020-08-31 16:55:34 +0200

    Dokumentace v url
    markdown > requirements
    description > url

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Django Chat
 
-Běžící aplikace na [Heroku](https://cryptic-plains-09325.herokuapp.com).
+Běžící aplikace na [Heroku](https://cryptic-plains-09325.herokuapp.com). 
 Administrace [/admin/](http://cryptic-plains-09325.herokuapp.com/admin/).
-Dokumentace API [/docs/](http://cryptic-plains-09325.herokuapp.com/docs/).
-Procházení API  [/schema/](http://cryptic-plains-09325.herokuapp.com/schema/).
+Dokumentace API [/docs/](http://cryptic-plains-09325.herokuapp.com/docs/). 
+Procházení API  [/chat/](http://cryptic-plains-09325.herokuapp.com/chat/).
 
 Účet administrátora admin/admin.
 Testovací účty a/a, b/b.
 
-Po registraci nového účtu je potřeba přidat mu v administraci kontaky do přátel, podobně jako je tomu na facebooku. Až poté je možné tyto přátele vyhledat a otevřít s nimi chat. Tato funkcionalita není implementována v React aplikaci.
+Po registraci nového účtu je potřeba přidat mu v administraci kontaky do přátel, podobně jako je tomu na facebooku. Až poté je možné tyto přátele vyhledat a otevřít s nimi chat. Tato funkcionalita není implementována v aplikaci React.
--- djangochat/urls.py
+++ djangochat/urls.py
@@ -5,19 +5,33 @@ from django.views.generic import TemplateView
 from rest_framework.documentation import include_docs_urls
 from rest_framework.schemas import get_schema_view
 
-schema_view = get_schema_view(title="Chat API", description="Chat API description", public=True)
 
 urlpatterns = [
-    path('schema/', schema_view),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('chat/', include('chat.api.urls', namespace='chat')),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
-    path('docs/', include_docs_urls(title='API doc')),
     path('', TemplateView.as_view(template_name='index.html')),
     # re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
     # path('docs/', include_docs_urls(title='API doc', public=False)),
+]
+
+
+# dokumentace
+schema_view = get_schema_view(
+    title="Chat API",
+    description="Chat API description",
+    public=True,
+)
+
+title='API Documentation'
+description="""
+This application was created for the WEAP course. Originally according to a tutorial on [YouTube](https://www.youtube.com/playlist?list=PLLRM7ROnmA9EnQmnfTgUzCfzbbnc-oEbZ).
+"""
+urlpatterns += [
+    path('schema/', schema_view),
+    path('docs/', include_docs_urls(title, description)),
 ]
 
 
